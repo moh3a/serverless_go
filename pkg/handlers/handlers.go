8b6025a6ad8e1a72ab84d50e6559a1f4af93062c
moh3a <aitabdelmalekma@gmail.com>
2023-05-20 23:28:05 +0100

    handling creating a user

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -36,8 +36,16 @@ func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dyn
 	return apiResponse(http.StatusOK, result)
 }
 
-func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
-	fmt.Printf("hello")
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
+	*events.APIGatewayProxyResponse, error,
+) {
+	result, err := user.CreateUser(req, tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusCreated, result)
 }
 
 func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayProxyResponse, error) {
